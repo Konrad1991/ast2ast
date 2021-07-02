2611973c4837e4af5ffa58d7780171fe1ca30761
KK <Konrad_kraemer@yahoo.de>
2021-07-02 16:54:15 +0200

    started identifying variables at rhs

--- R/controlblocks.R
+++ R/controlblocks.R
@@ -50,15 +50,52 @@ arguments <- function(vars) {
 vars$list <- arguments("num(x), num(y)")
 
 # ================================================================================
+# fct which loops over lines
+# ================================================================================
+loop <- function(code_lines, fct) {
+  index <- parent.frame()$i[]
+  res <- lapply(code_lines, fct)
+  
+  return(res)
+}
+
+# ================================================================================
+# fct which finds '<-'
+# ================================================================================
+find_assign <- function(element_one) {
+  if(element_one == rlang::sym("<-")) {
+    return(TRUE)
+  } else {
+    return(FALSE)
+  }
+}
+
+# ================================================================================
+# fct which finds line with '<-'
+# ================================================================================
+find_assign_lines <- function(code) {
+  
+  fct <- function(code_line) {
+    if(find_assign(code_line[[1]]) == TRUE) {
+      return(TRUE)
+    } else {
+      return(FALSE)
+    }
+  }
+  
+  res <- loop(code, fct)
+}
+
+
+# ================================================================================
 # examine one line update var$list if necessary
 # ================================================================================
+# does not work
 check_symbols <- function(symb) {
   allowed_fct <- c("+", "-", "*", "/")
   
   check <- "var"
 
-  print(symb)
-  print("_:")
   for(i in seq_along(allowed_fct)) {
     if(symb == allowed_fct[i]) {
       check <- "fct"
@@ -70,39 +107,45 @@ check_symbols <- function(symb) {
 }
 
 
-
-newvar <- function(codeast) {
-  out <- lapply(codeast, function(x) {
+# ================================================================================
+# fct which finds variables at rhs
+# ================================================================================
+check_rhs <- function(code, assign_lines) {
+  
+  ret <- list()
+  counter <- 0
+  for(i in 1:length(code)) {
     
-    var_list_rhs <- list()
-    counter <- 1
-    
-    type <- lapply(codeast, function(y) {
+    if(assign_lines[[i]] == TRUE) {
+      counter <- counter + 1
+      ret[[counter]] <- lapply(code[[i]][2:length(code[[i]])], function(x) {
+        #test <- check_symbols(x) # does not work
+        if(test == "var") {
+          return(x)
+        } else {
+          return("novar")}
+      } )
       
-      if(x[[1]] == rlang::sym("<-")) {
-        
-        variables_at_rhs <- lapply(y[2:length(y)], function(x) {
-          test <- check_symbols(x)
-          if(test == "var") {
-            elem_index <- grep(quote(x), vars$list)
-            var_list_rhs[[counter]] <<- vars$list[[elem_index]]  
-          }
-        })
-        
-      }
-      
-    })
-      
-    })
-
+    }
+  }
+  
+  return(ret)
+  
 }
 
-# one fctloopüing over lines
-# fct which examines different lines
 
-code <- "y <- x + 1"
+#if(test == "var") {
+# elem_index <- grep(quote(x), vars$list)
+#var_list_rhs[[counter]] <<- vars$list[[elem_index]]
+#}
+
+code <- "y <- x + 1
+         y <- y + 1
+         print(y)"
 test <- sep_expressions(code)
 test <- extractast(test)
+assign_lines<-find_assign_lines(test)
+vars <- check_rhs(test, assign_lines)
 vars <- new.env()
 vars$list <- list()
 vars$list <- arguments("num(x), num(z)")
