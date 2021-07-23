3b9ebb2bdbb28ce3b15934a256215ed84ad85060
KK <Konrad_kraemer@yahoo.de>
2021-07-23 07:59:33 +0200

    added file which defines a basic example in order to know how the functions have to be used in which order. Later this is the 'main' function called by the user

---
+++ R/flow.R
@@ -0,0 +1,27 @@
+#setwd("/home/konrad/Dokumente/1Privat/Programming/AST")
+
+#Rcpp::compileAttributes("ast2ast")
+
+#install.packages("ast2ast", repos = NULL, type = "source")
+
+
+#detach("package:AstToAst", unload = TRUE)
+#library(AstToAst)
+
+input_variables <- list(y = "vec_num", x = "num") # y = vec_num und x = scalar num
+code <- "y <- y + x
+         y <- y + 1
+         print(y)"
+test <- sep_expressions(code)
+test <- extractast(test)
+get_assignments(test)
+vars <- storage_variables(test)
+vars
+temp <- unlist(test[[2]][3])
+find_var_at_rhs(as.character(temp))
+vars_rhs <- variables_at_rhs(test)
+vars_rhs
+vars_list <- translate(test, input_variables)
+vars_list
+types(vars_list, test)
+new_(vars_list, test)
