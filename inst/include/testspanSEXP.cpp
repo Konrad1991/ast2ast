19d60eb9a410a2318fc3119f793ba183c65ae82c
Konrad <35558461+Konrad1991@users.noreply.github.com>
2021-09-24 17:36:40 +0200

    Update testspanSEXP.cpp

--- inst/include/testspanSEXP.cpp
+++ inst/include/testspanSEXP.cpp
@@ -31,16 +31,38 @@ SEXP test(SEXP a)
   R::Protect ab(R::allocVectorNumeric(na));
   p = R::numericPointer(ab);
   
-  SPAN s(xa, na);
+  SPAN s(p, na);
   
   for(int i = 0; i < na; i++) {
     s[i] = static_cast<double>(i) + 20.;
-    p[i] = s[i];
+    //p[i] = s[i];
   }
   
   return ab;
 }
 
+
+
+// [[Rcpp::export]]
+void test2(SEXP a)
+{
+  int na;
+  double *xa;
+  
+  R::Protect pa(R::coerceVectorNumeric(a));
+  na = R::length(pa); 
+  
+  SPAN s(REAL(a), na);
+  for(int i = 0; i < na; i++) {
+    s[i] = static_cast<double>(i) + 20.;
+  }
+  
+}
+
+
 /*** R
 test(c(1,2,3))
+x <- c(1,2,3)
+test2(x)
+x
 */
