5cb9cb12e595e889ff3c17b20ecb4cabc2ff6678
Konrad <35558461+Konrad1991@users.noreply.github.com>
2021-09-24 16:20:41 +0200

    updated span

--- inst/include/testspan.cpp
+++ inst/include/testspan.cpp
@@ -1,81 +1,14 @@
 #include <iostream>
 #include <memory>
 
-class VEC {
-  public:
-    double* data;
-    int size;
-  VEC(double* p_, int size_) : size(size_) {
-    data = new double[size_];
-    for(int i = 0; i < size_; i++) {
-      data[i] = p_[i];
-    }
-  }
-
-  ~VEC() {
-    delete[] data;
-  }
-
-  double operator[](int i) {
-    return data[i];
-  }
-};
 
 class SPAN {
-  public:
-  double** sp;
-  int size;
-  SPAN(double* p, int size_) : size(size_) {
-    sp = &p;
-  }
 
-  double operator[](int i) {
-    double temp = **sp+i;
-    return *sp[i];
-  }
-
-};
-
-
-// works
-class SPAN2 {
-  public:
-  double*& sp;
-  int size;
-  SPAN2(double*& p, int size_) : size(size_), sp(p) {}
-
-  double operator[](int i) {
-    return *(sp + i);
-  }
-
-  SPAN2 operator=(double inp) {
-    *sp = inp;
-    return *this;
-  }
-
-};
-
-
-
-// works
-class SPAN3 {
-  public:
   double* sp;
   int size;
-  double* current;
-  SPAN3(double* p, int size_) : size(size_), sp(p) {}
 
-/*
-  double operator[](int i) {
-    return sp[i];
-  }
-*/
-
-/*
-  double* operator[](int i) {
-    return &sp[i];
-  }
-*/
+  public:
+  SPAN(double* p, int size_) : size(size_), sp(p) {}
 
   double& operator[](int i) {
     return sp[i];
@@ -92,47 +25,10 @@ int main() {
     p[i] = static_cast<double>(i);
   }
 
-  VEC v(p, n);
-  SPAN s(p, n);
-  SPAN2 s2(p,n);
+  SPAN sp(p, n);
 
-  for(int i = 0; i < v.size; i++) {
-    //std::cout << v[i] << std::endl;
-  }
-
-  p[0] = 30;
-  double*& ref = p;
-  //std::cout << *(s2.sp + 0) << std::endl;
-
-  s2 = 70;
-  s2.sp[1] =34567;
-  //std::cout << ref[0]<< " " << p[0] << " " << s2[0] + s2[1] << std::endl;
-
-  v.data[3] = 345;
-  //std::cout << v[3] << std::endl;
-
-  span<double> s_new(p, n);
-
-  s_new.ptr_[5] = 45;
-
-  for (auto i : s_new) {
-    //std::cout << i << "\n";
-  }
-
-  double* sp = p;
-
-  for(int i = 0; i < n; i++) {
-    sp[i] = i + i;
-    std::cout << sp[i] << " " << p[i] << std::endl;
-  }
-
-  SPAN3 sp3(p, n);
-
-  //*sp3[3] = 100;
-  //std::cout << *sp3[3] << " " << p[3] << std::endl;
-
-  sp3[3] = 80;
-  std::cout << sp3[3] << " " << p[3] << std::endl;
+  sp[3] = 80;
+  std::cout << sp[3] << " " << p[3] << std::endl;
 
   delete[] p;
 }
