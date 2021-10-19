b0a4198e773b40c875dbf906e8c6dd41185eee55
Konrad <35558461+Konrad1991@users.noreply.github.com>
2021-10-19 13:34:35 +0200

    Create convert_fct_to_string.R

---
+++ R/convert_fct_to_string.R
@@ -0,0 +1,37 @@
+# R package ast2ast
+# Copyright (C) 2021 Konrad Krämer
+# 
+# This file is part of R package ast2ast
+# 
+# 
+# ast2ast is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along with ast2ast
+# If not see: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html#SEC4
+
+# ================================================================================
+# function to convert function in strings
+# ================================================================================
+c2s <- function(fct) {
+  d <- body(fct)
+  code <- list()
+  for(i in 2:length(d)) {
+    code[[i-1]] <- toString(d[i])
+  }
+  return(code)
+}
+
+
+# Example
+# c2s(test <- function(a) {print("a")
+#   a <- a + 1
+#   print(a)}
+#   )
