293e8932753f4847ba07bc4d432d0300800fd474
Konrad <Konrad_kraemer@yahoo.de>
2022-09-03 11:24:52 +0200

    wrong include path removed

--- inst/include/etr.hpp
+++ inst/include/etr.hpp
@@ -43,7 +43,7 @@ If not see: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html#SEC4
 #include "etr_bits/comparison.hpp"
 #include "etr_bits/looping.hpp"
 #include "etr_bits/checks_na_inf.hpp"
-#include "etr_bits/distri/distri.hpp"
+#include "etr_bits/distri.hpp"
 
 //#define vec VEC<double>
 //#define mat VEC<double>
--- vignettes/DetailedDocumentation_cache/html/unnamed-chunk-2_sourceCpp/sourceCpp-x86_64-pc-linux-gnu-1.0.8.3/sourcecpp_661e7deaa11e/file661e4f5cd44.cpp.R
+++
@@ -1,5 +0,0 @@
-`.sourceCpp_1_DLLInfo` <- dyn.load('/home/konrad/Documents/GitHub/ast2ast_supplement/ast2ast/vignettes/DetailedDocumentation_cache/html/unnamed-chunk-2_sourceCpp/sourceCpp-x86_64-pc-linux-gnu-1.0.8.3/sourcecpp_661e7deaa11e/sourceCpp_5.so')
-
-byref <- Rcpp:::sourceCppFunction(function(fetr, x) {}, TRUE, `.sourceCpp_1_DLLInfo`, 'sourceCpp_1_byref')
-
-rm(`.sourceCpp_1_DLLInfo`)
