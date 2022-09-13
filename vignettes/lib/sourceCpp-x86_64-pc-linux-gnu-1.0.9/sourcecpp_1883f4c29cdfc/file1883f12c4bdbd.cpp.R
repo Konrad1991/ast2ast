c6300a740ec51a36376909197c204c7a4ac687cd
Konrad <Konrad_kraemer@yahoo.de>
2022-09-13 16:25:10 +0200

    update docu

--- vignettes/lib/sourceCpp-x86_64-pc-linux-gnu-1.0.9/sourcecpp_1883f4c29cdfc/file1883f12c4bdbd.cpp.R
+++
@@ -1,5 +0,0 @@
-`.sourceCpp_1_DLLInfo` <- dyn.load('/home/konrad/Documents/GitHub/ast2ast_supplement/ast2ast/vignettes/lib/sourceCpp-x86_64-pc-linux-gnu-1.0.9/sourcecpp_1883f4c29cdfc/sourceCpp_2.so')
-
-getXPtr <- Rcpp:::sourceCppFunction(function() {}, FALSE, `.sourceCpp_1_DLLInfo`, 'sourceCpp_1_getXPtr')
-
-rm(`.sourceCpp_1_DLLInfo`)
