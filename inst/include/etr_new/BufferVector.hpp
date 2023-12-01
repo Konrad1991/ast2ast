1c5c884a5379327de5a0867bd440068f43e86581
Konrad1991 <konrad_kraemer@yahoo.de>
2023-12-01 10:48:11 +0100

    added the operator= for rcpp and armadillo. eval=FALSE for the vignette for the old pointer interface

--- inst/include/etr_new/BufferVector.hpp
+++ inst/include/etr_new/BufferVector.hpp
@@ -377,7 +377,38 @@ template <typename T, typename R, typename Trait> struct Vec {
     return os;
   }
 
-  // issue: operator= is missing for Rcpp and armadillo
+  Vec& operator=(Rcpp::NumericVector& otherVec) {
+    d.resize(static_cast<size_t>(otherVec.size()));
+    d.mp.setMatrix(false, 0, 0);
+    for (size_t i = 0; i < otherVec.size(); i++) {
+      d[i] = otherVec[i];
+    }
+    return *this;
+  }
+  Vec& operator=(Rcpp::NumericMatrix& otherVec) {
+    d.resize(static_cast<size_t>(otherVec.size()));
+    d.mp.setMatrix(true, otherVec.nrow(), otherVec.ncol());
+    for (size_t i = 0; i < otherVec.size(); i++) {
+      d[i] = otherVec[i];
+    }
+    return *this;
+  }
+  Vec& operator=(arma::vec& otherVec) {
+    d.resize(static_cast<size_t>(otherVec.size()));
+    d.mp.setMatrix(false, 0, 0);
+    for (size_t i = 0; i < otherVec.size(); i++) {
+      d[i] = otherVec[i];
+    }
+    return *this;
+  }
+  Vec& operator=(arma::mat& otherVec) {
+    d.resize(static_cast<size_t>(otherVec.size()));
+    d.mp.setMatrix(true, otherVec.n_rows, otherVec.n_cols);
+    for (size_t i = 0; i < otherVec.size(); i++) {
+      d[i] = otherVec[i];
+    }
+    return *this;
+  }
   Vec(Rcpp::NumericVector otherVec)
       : d() {
     d.resize(static_cast<size_t>(otherVec.size()));
