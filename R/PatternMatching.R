3fd8970c50c488700ad091bb441a4ac546154e67
Konrad1991 <konrad_kraemer@yahoo.de>
2024-05-02 10:02:37 +0200

    updated the pattern matching mechanism

--- R/PatternMatching.R
+++ R/PatternMatching.R
@@ -1,199 +1,51 @@
 setClass("Match",
-  slots = list(arg = "ANY", exprResult = "ANY", exprEvaluated = "logical")
+         slots = list(arg = "ANY")
 )
 
 match <- function(x) {
-  return(new("Match", arg = x, exprResult = NULL, exprEvaluated = FALSE))
+  return(new("Match", arg = x))
 }
 
-setGeneric("when", function(e1, e2, expr) {
-  standardGeneric("when")
-})
-
-
-setMethod(
-  "when",
-  signature(e1 = "Match", e2 = "ANY", expr = "ANY"), function(e1, e2, expr) {
-    if (e1@exprEvaluated) {
-      return(e1)
-    }
-    if (identical(slot(e1, "arg"), e2)) {
-      e1@exprResult <- eval(expr)
-      e1@exprEvaluated <- TRUE
-      return(e1)
-    } else {
-      return(match(e1))
-    }
+when <- function(value, test, predicate, apply = FALSE) {
+  if (is.null(test) && inherits(value, "Match")) {
+    return(predicate)
   }
-)
-
-setMethod(
-  "when",
-  signature(e1 = "ANY", e2 = "ANY", expr = "ANY"), function(e1, e2, expr) {
-    if (identical(e1, e2)) {
-      return(new("Match",
-        arg = e1,
-        exprResult = eval(expr), exprEvaluated = TRUE
-      ))
-    } else {
-      return(match(e1))
-    }
+  if (!inherits(value, "Match")) {
+    return(value)
   }
-)
-
-setGeneric("test", function(e1, e2, expr, apply = FALSE) {
-  standardGeneric("test")
-})
-
-setMethod(
-  "test",
-  signature(e1 = "Match", e2 = "ANY", expr = "ANY"),
-  function(e1, e2, expr, apply = FALSE) {
-    if (e1@exprEvaluated) {
-      return(e1)
-    }
-    if (do.call(e2, list(e1@arg))) {
-      e1@exprResult <- eval(expr)
-      e1@exprEvaluated <- TRUE
-      return(e1)
-    } else {
-      return(match(e1))
-    }
-  }
-)
-
-setMethod(
-  "test",
-  signature(e1 = "ANY", e2 = "ANY", expr = "ANY"),
-  function(e1, e2, expr, apply = FALSE) {
-    if (do.call(e2, list(e1))) {
-      return(new("Match",
-        arg = e1,
-        exprResult = eval(expr), exprEvaluated = TRUE
-      ))
-    } else {
-      return(match(e1))
-    }
-  }
-)
-
-setMethod(
-  "test",
-  signature(e1 = "Match", e2 = "ANY", expr = "ANY", apply = "logical"),
-  function(e1, e2, expr, apply) {
-    if (e1@exprEvaluated) {
-      return(e1)
-    }
-    if (do.call(e2, list(e1@arg))) {
+  if (is.function(test)) {
+    if (do.call(test, list(value@arg))) {
       if (apply) {
-        e1@exprResult <- do.call(expr, list(e1@arg))
+        if (is.function(predicate)) {
+          return(do.call(predicate, list(value@arg)))
+        } else {
+          warning("apply is true but predicate is not a function. value is returned.")
+          return(predicate)
+        }
       } else {
-        e1@exprResult <- eval(expr)
-      }
-      e1@exprEvaluated <- TRUE
-      return(e1)
-    } else {
-      return(match(e1))
-    }
-  }
-)
-
-setMethod(
-  "test",
-  signature(e1 = "ANY", e2 = "ANY", expr = "ANY", apply = "logical"),
-  function(e1, e2, expr, apply) {
-    if (do.call(e2, list(e1))) {
-      if (apply) {
-        return(new("Match",
-          arg = e1,
-          exprResult = do.call(expr, list(e1)), exprEvaluated = TRUE
-        ))
-      } else {
-        return(new("Match",
-          arg = e1,
-          exprResult = eval(expr), exprEvaluated = TRUE
-        ))
+        return(predicate)
       }
     } else {
-      return(match(e1))
+      return(value)
     }
+  } else if (identical(slot(value, "arg"), test)) {
+    return(eval(predicate))
+  } else {
+    return(value)
   }
-)
+}
 
-setGeneric("g", function(e) {
-  standardGeneric("g")
-})
+result <- match("+") |>
+  when("-", "minus") |>
+  when(is.numeric, "bla") |> 
+  when(is.character, "plus") |> 
+  when(NULL, "not found")
 
-setMethod(
-  "g",
-  signature(e = "Match"),
-  function(e) {
-    if (e@exprEvaluated) {
-      return(e@exprResult)
-    } else {
-      return("NotFound")
-    }
-  }
-)
+result
 
-setMethod(
-  "g",
-  signature(e = "ANY"),
-  function(e) {
-    return(e)
-  }
-)
+result <- match(1L) |>
+  when("-", "minus") |>
+  when(is.character, "plus") |> 
+  when(NULL, "not found")
 
-# "+" |>
-#   when("+", "plus") |>
-#   g()
-# 
-# "-" |>
-#   when("+", "plus") |>
-#   when("-", "minus") |>
-#   g()
-# 
-# match("+") |>
-#   when("+", "+") |>
-#   when("-", "-") |>
-#   when("_", "default") |>
-#   g()
-# 
-# "+" |>
-#   test(is.character, "asdfsdfgh") |>
-#   test(is.numeric, 123) |>
-#   g()
-# 
-# "+" |>
-#   when("+", "+") |>
-#   when("-", "-") |>
-#   when("_", "default") |>
-#   g()
-# 
-# predicate <- function(a) {
-#   if (a[[1]] == "-") {
-#     return(TRUE)
-#   }
-#   return(FALSE)
-# }
-# 
-# convert <- function(a) {
-#   if (a[[1]] == "-" && length(a) == 3) {
-#     return("minus")
-#   } else if (a[[1]] == "-" && length(a) == 2) {
-#     return("Unaryminus")
-#   }
-# }
-# 
-# list("+", 1, 2) |>
-#   test(predicate, convert, TRUE) |>
-#   g()
-# 
-# 
-# list("-", 1, 2) |>
-#   test(predicate, convert, TRUE) |>
-#   g()
-# 
-# list("-", 1) |>
-#   test(predicate, convert, TRUE) |>
-#   g()
+result
