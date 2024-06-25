b4dc0d0832d25862351ed4eda6ac2df7f57d38aa
konrad.kraemer <konrad.kraemer@kit.edu>
2024-06-25 20:06:06 +0200

    defined function which defines the signature

--- R/Signature.R
+++ R/Signature.R
@@ -1,14 +1,8 @@
-SignatureClass <- R6::R6Class("SignatureClass",
-  public = list(
-    types_of_arguments = NULL,
-    original_argument_names = NULL,
-    new_argument_names = NULL,
-    return_type = NULL,
-    fct_name = NULL,
-    initialize = function(toa, oan, fct_name) {
-      self$types_of_arguments <- toa
-      self$original_argument_names <- oan
-      self$fct_name <- fct_name
-    }
-  )
-)
+signature_code <- function(return_type, fct_name,
+                           types_of_arguments, names) {
+  cString(
+    return_type, " ", fct_name, "(",
+    paste(types_of_arguments, names, collapse = ", "),
+    ") {\n", ""
+  )@value
+}
