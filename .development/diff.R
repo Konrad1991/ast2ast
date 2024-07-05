5b886d3433870cd828bbd3858b592b7f06f87f8e
Konrad1991 <konrad_kraemer@yahoo.de>
2024-07-05 13:17:20 +0200

    update c approach

--- .development/diff.R
+++ .development/diff.R
@@ -1,16 +1,15 @@
+f <- function(a) {
+  print(a)
+}
+
 library(ast2ast)
 
-f <- function() {
-  a::logical <- TRUE
-  b::integer <- 1
-  c::double <- 3.14
-  d::logical_vector <- c(TRUE, FALSE)
-  e::integer_vector <- c(1L, 2L, 3L)
-  f::double_vector <- c(3.14, 3.5)
-}
-library(ast2ast)
 fcpp <- translate(f,
-  verbose = TRUE
+  types_of_args = "const double",
+  verbose = TRUE,
+  getsource = TRUE,
+  output = "XPtr",
+  handle_inputs = ""
 )
 
-fcpp()
+fcpp |> cat()
