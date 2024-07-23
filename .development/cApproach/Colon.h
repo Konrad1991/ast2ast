680cc5c06f7c2c880af3d632a380ef91a297c114
Konrad1991 <konrad_kraemer@yahoo.de>
2024-07-23 14:15:54 +0200

    c appriach now in own branch

--- .development/cApproach/Colon.h
+++
@@ -1,9 +0,0 @@
-#include "VectorManager.h"
-
-#ifndef COLON_H
-#define COLON_H
-
-void colon_numeric(size_t vec_index, double start, double end,
-                   VectorManager *vm);
-
-#endif
--- .development/cApproach/header.h
+++
@@ -1,9 +0,0 @@
-#ifndef HEADER_CAPPROACH_H
-#define HEADER_CAPPROACH_H
-
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#endif
