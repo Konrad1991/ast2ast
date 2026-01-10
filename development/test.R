5a5959583c35e5034c84ed71bcc7bfe63c4011ad
Konrad1991 <konrad_kraemer@yahoo.de>
2026-01-10 10:09:00 +0100

    update vignettes

--- development/test.R
+++ development/test.R
@@ -3,3 +3,14 @@ system('find -name "*.so" | xargs rm')
 Rcpp::compileAttributes()
 install.packages(".", types = "source", repo = NULL,)
 tinytest::test_package("ast2ast")
+
+rmarkdown::render("vignettes/DetailedDocumentation.Rmd")
+
+f <- function() {
+  a |> type(int)
+  a <- 3.14
+  return(a)
+}
+
+fcpp <- ast2ast::translate(f)
+fcpp()
